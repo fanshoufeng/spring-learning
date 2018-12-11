e881b35af172d8fc54c0a07301111c8c03123111
Michael Fan <shoufeng.fan@gmail.com>
2018-12-11 20:59:47 +0800

    Submit eureka-server Project

---
+++ spring-learning-parent/spring-cloud/eureka-server/src/main/java/org/fanshoufeng/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package org.fanshoufeng.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Michael Fan
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
