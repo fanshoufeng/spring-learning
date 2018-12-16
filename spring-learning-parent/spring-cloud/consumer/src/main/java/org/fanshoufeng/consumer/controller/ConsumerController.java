71d43b8d14dca443cfecb1f335c90a80c311e007
Michael Fan <shoufeng.fan@gmail.com>
2018-12-16 16:10:42 +0800

    New consumer Project

---
+++ spring-learning-parent/spring-cloud/consumer/src/main/java/org/fanshoufeng/consumer/controller/ConsumerController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.fanshoufeng.consumer.controller;
+
+import org.fanshoufeng.consumer.remote.HelloRemote;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Michael Fan
+ *
+ */
+@RestController
+public class ConsumerController {
+
+	@Autowired
+	HelloRemote helloRemote;
+	
+	@RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return helloRemote.hello(name);
+    }
+	
+}
---
+++ spring-learning-parent/spring-cloud/consumer/src/main/java/org/fanshoufeng/consumer/remote/HelloRemote.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.fanshoufeng.consumer.remote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Michael Fan
+ *
+ */
+@FeignClient(name= "producer")
+public interface HelloRemote {
+
+	@RequestMapping(value = "/hello")
+    public String hello(@RequestParam(value = "name") String name);
+	
+}
