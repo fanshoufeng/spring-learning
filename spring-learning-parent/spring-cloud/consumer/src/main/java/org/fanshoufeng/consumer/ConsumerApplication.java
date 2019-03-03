19bf837c8dab68090a28ae69ceae79d7ccdda8dc
Michael Fan <shoufeng.fan@gmail.com>
2019-03-03 18:47:41 +0800

    @EnableHystrixDashboard @EnableCircuitBreaker

--- spring-learning-parent/spring-cloud/consumer/src/main/java/org/fanshoufeng/consumer/ConsumerApplication.java
+++ spring-learning-parent/spring-cloud/consumer/src/main/java/org/fanshoufeng/consumer/ConsumerApplication.java
@@ -2,9 +2,13 @@ package org.fanshoufeng.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
